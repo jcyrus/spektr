0933e60c0542845827b01ae252ba6029d4b55919
Cyrus Espiritu <jcyrus28@gmail.com>
2026-01-07 22:21:58 +0700

    chore(packaging): update SHA256 hashes for Scoop and Homebrew package manifests

--- homebrew/spektr.rb
+++ homebrew/spektr.rb
@@ -7,20 +7,20 @@ class Spektr < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/jcyrus/spektr/releases/download/v0.1.0/spektr-v0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256_AARCH64"
+      sha256 "a3eefe2f53bf6ce3cdf4b350a99d4ec7e6e9488f7db98e0a28514719e93550e1"
     else
       url "https://github.com/jcyrus/spektr/releases/download/v0.1.0/spektr-v0.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256_X86_64"
+      sha256 "bdc43805fe9b8ee4fc8f5fa7a882d7f147e2460ddeb2c933407cedb422d3e77c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/jcyrus/spektr/releases/download/v0.1.0/spektr-v0.1.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256_LINUX_ARM"
+      sha256 "ea479c1981895e0843f62644457acb6fe848229190694951a7b297fd6bb09499"
     else
       url "https://github.com/jcyrus/spektr/releases/download/v0.1.0/spektr-v0.1.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256_LINUX_X86"
+      sha256 "184722a9d14a7c8969a0f3bbf204386cd72d0c861636ce31016bcf3db2e9a142"
     end
   end
 
