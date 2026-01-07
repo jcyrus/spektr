f42ccdb0844f3677a7a59d3ffd99de552e649683
Cyrus Espiritu <jcyrus28@gmail.com>
2026-01-07 22:14:37 +0700

    chore(packaging): update release artifact URLs to include 'v' prefix in filenames

--- homebrew/spektr.rb
+++ homebrew/spektr.rb
@@ -6,20 +6,20 @@ class Spektr < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jcyrus/spektr/releases/download/v0.1.0/spektr-0.1.0-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/jcyrus/spektr/releases/download/v0.1.0/spektr-v0.1.0-aarch64-apple-darwin.tar.gz"
       sha256 "REPLACE_WITH_ACTUAL_SHA256_AARCH64"
     else
-      url "https://github.com/jcyrus/spektr/releases/download/v0.1.0/spektr-0.1.0-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/jcyrus/spektr/releases/download/v0.1.0/spektr-v0.1.0-x86_64-apple-darwin.tar.gz"
       sha256 "REPLACE_WITH_ACTUAL_SHA256_X86_64"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/jcyrus/spektr/releases/download/v0.1.0/spektr-0.1.0-aarch64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/jcyrus/spektr/releases/download/v0.1.0/spektr-v0.1.0-aarch64-unknown-linux-gnu.tar.gz"
       sha256 "REPLACE_WITH_ACTUAL_SHA256_LINUX_ARM"
     else
-      url "https://github.com/jcyrus/spektr/releases/download/v0.1.0/spektr-0.1.0-x86_64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/jcyrus/spektr/releases/download/v0.1.0/spektr-v0.1.0-x86_64-unknown-linux-gnu.tar.gz"
       sha256 "REPLACE_WITH_ACTUAL_SHA256_LINUX_X86"
     end
   end
